f85ba361bbbc56fc0a6e6a20f2b7dcf536a56d38
vandanparmar <vandan@hotmail.co.uk>
2017-07-04 22:44:43 -0700

    adding loading from GUI and loading / saving to / from file.

--- graph_tools.py
+++ graph_tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import json
 
 
 #generate a random graph with a given number of nodes and probability for each edge.
@@ -17,6 +18,42 @@ def show_graph(adjacency_matrix):
     gr = nx.Graph()
     gr.add_edges_from(edges)
     # nx.draw(gr) # edited to include labels
-    nx.draw(gr)
+    nx.draw_spring(gr)
     plt.show() 
 
+
+def save_to_file(adjacency_matrix,filename):
+	np.savetxt(filename,adjacency_matrix)
+	print('File saved!')
+
+
+def load_from_file(filename):
+	if filename.endswith('.json'):
+		toReturn = load_from_GUI(filename)
+	if filename.endswith(('.txt','.dat')):
+		toReturn = load_from_numpy(filename)
+	return toReturn
+
+
+def load_from_GUI(filename):
+	with open(filename) as data_file:    
+		data = json.load(data_file)
+		nodes_num = len(data['nodes'])
+		toReturn = np.zeros((nodes_num,nodes_num))
+		nodes_map = {}
+		for i,node in enumerate(data['nodes']):
+			nodes_map[node] = i
+		for edge in data['edges']:
+			toReturn[nodes_map[edge['source']]][nodes_map[edge['target']]]+=1
+		toReturn += np.transpose(toReturn)
+		toReturn = np.clip(toReturn,0.0,1.0)
+		return toReturn
+
+
+def load_from_numpy(filename):
+	toReturn = np.loadtxt(filename)
+	return toReturn
+
+#save_to_file(load_from_GUI('test_graph_1.json'),'test_graph_2.txt')
+#show_graph(load_from_file('test_graph_2.txt'))
+#show_graph(load_from_file('test_graph_1.json'))
