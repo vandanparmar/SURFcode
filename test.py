d885d859edc33b096b4340074f19762bce36b4db
vandanparmar <vandan@hotmail.co.uk>
2017-07-06 10:45:47 -0700

    completed discrete simulation

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 from simulation_tools import *
 
-test_1 = simulate_cont(5,5)
-A = np.array([[-1,0],[0,-1]])
-B = np.array([[1,0],[1,0],[1,0],[1,0]])
-C = np.array([[1,0],[0,1]])
-#test_1.setABC(A,C,B)
+# test_1 = simulate_cont(5,5)
+# A = np.array([[-1,0],[0,-1]])
+# B = np.array([[1,0],[1,0],[1,0],[1,0]])
+# C = np.array([[1,0],[0,1]])
+# #test_1.setABC(A,C,B)
 
-x = test_1.get_x(5)
-y = test_1.get_y(5)
+# x = test_1.get_x(5)
+# y = test_1.get_y(5)
 
-test_1.plot([500,510])
+# test_1.plot([500,510])
+
+test = simulate_disc()
+test.plot([1,10])
